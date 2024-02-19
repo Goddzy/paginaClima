29529b49b9d29da61d030030afce5cdd65a34294
Renato Trevisiol Montiel <renatotrevisiolmontiel@gmail.com>
2024-02-18 22:48:02 -0300

    realicé peticiones a la url y pude extraer y usar los datos

--- js/app.js
+++ js/app.js
@@ -1,20 +1,45 @@
 let lon;
 let lat;
+let fechaActual = document.getElementById('fechaActual')
 let temperatura = document.getElementById('temperatura')
-let icono = document.getElementById('icono')
 let localidad = document.getElementById('localidad')
+let urlBase;
+
 
 window.addEventListener('load', ()=>{
     
  if(navigator.geolocation){
     navigator.geolocation.getCurrentPosition((position)=>{
         //obtengo la localización del navegador
-        console.log(position)
         //position será un objeto que tendrá varios datos, entre los que se encuentran la latitud y longitud, al obtener dichos datos, redefino las variables lon y lat y guardo dichos datos en las variables.
         lon = position.coords.longitude
         lat = position.coords.latitude
 
+        //api sacada de openweathermap.org
+        const api = 'f3f25df4cf6424a6d7095df2d61bcf79'
+        //aquí guardo en una constante la url con la latitud, longitud, y el api 
+        urlBase = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api}`
+        consultarApi()
     })
  }
 
+
+
 })
+
+let consultarApi = async()=>{
+    try{
+        //hacer la consulta al API
+        const respuesta = await fetch(urlBase);
+        if(!respuesta.ok) {
+            throw new Error('La solicitud no fue exitosa');
+        }
+        const dato = await respuesta.json();
+        console.log(dato)
+        //reemplazar las variables por los datos
+        localidad.textContent = dato.name + ", " + dato.sys.country
+        temperatura.textContent = Math.floor(dato.main.temp - 273.15) + '°C'
+    } catch(error){
+        console.log(error);
+    }
+}
